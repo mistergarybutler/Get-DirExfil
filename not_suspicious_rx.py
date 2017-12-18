6be493e4784426801dcf73c7bfbf70bde8fecae3
mistergarybutler <31485993+mistergarybutler@users.noreply.github.com>
2017-12-17 22:22:07 -0500

    Add files via upload
    Fixed typos. Implemented some data received checks. Discovered data loss.

--- not_suspicious_rx.py
+++ not_suspicious_rx.py
@@ -3,10 +3,10 @@
 import os, sys, socket, random
 
 randomInt = random.random()
-filename = "a" + str(random).split(".")[1] + ".loot"
+filename = "a" + str(randomInt).split(".")[1] + ".loot"
 fd = open(filename, "w")
 
-if(len(sys.argv != 4):
+if(len(sys.argv) != 4):
 	print "Please supply args: rhost rport buffersize"
 	sys.exit()
 	
@@ -19,13 +19,22 @@ s.bind((rhost, rport))
 s.listen(1)
 while True:
 	connection, address = s.accept()
+	print type(connection)
+	print address
 	
-	result = connection.recv(buf + 3)
+	count=0
+	bytes=0	
+	result = ' '
 	while result != '':
-		result = connection.recv(buf + 3)
+		result = connection.recv(buf + 100)
+
 		if(len(result) == buf + 3):
+			print len(result)
 			s.close()
 			fd.close()
 			sys.exit()
 		elif(len(result) <= buf):
-			f.write(result)
+			fd.write(result)
+#			count+=1
+#			bytes+=len(result)
+#			print "Data packet: " + str(count) + " Bytes: " + str(bytes)
